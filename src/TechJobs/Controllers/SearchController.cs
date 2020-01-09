a6be3127a8d597319f370e84ac80ee3fb7100fb5
Noah Garrett <garretns@gmail.com>
2020-01-09 09:36:38 -0600

    dab styling sheet wont work sad dab

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -15,6 +15,23 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+        public IActionResult Results(string SearchTerm, string SearchType)
+        {
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.title = "Search";
+
+            if (SearchType == "all")
+            {
+                ViewBag.jobs = JobData.FindByValue(SearchTerm);    
+            }
+            else
+            {
+                ViewBag.jobs = JobData.FindByColumnAndValue(SearchType, SearchTerm);
+            }
+            // else look up how we did it in the previous one.
+            //probs search ListController.SearchType via SearchTerm or something along those lines
+            return View("Index");
+        }
 
     }
 }
